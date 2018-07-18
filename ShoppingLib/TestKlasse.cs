142eb1c07138bfc311597f41fe621aaaba5a2412
Rhoktar <tim.kob@protonmail.com>
2018-07-19 00:15:56 +0200

    Projekt Klassenbibliothek ShoppingLib zugefügt Projekt BackendTesting zugefügt

---
+++ ShoppingLib/TestKlasse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ShoppingLib
+{
+    public class TestKlasse
+    {
+        public void DatenAbrufen()
+        {
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "Einkauf";
+            if (dbCon.IsConnect())
+            {
+                //suppose col0 and col1 are defined as VARCHAR in the DB
+                string query = "SELECT name,category FROM items";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string someStringFromColumnZero = reader.GetString(0);
+                    string someStringFromColumnOne = reader.GetString(1);
+                    Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                }
+                dbCon.Close();
+            }
+        }
+        public void DatenSchreiben()
+        {
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "Einkauf";
+            if (dbCon.IsConnect())
+            {
+                //suppose col0 and col1 are defined as VARCHAR in the DB
+                string query = "INSERT INTO items(name, amount, category) VALUES (@name, @amount, @category)";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                cmd.Parameters.AddWithValue("@name", "Bananen");
+                cmd.Parameters.AddWithValue("@amount", "6 Stk");
+                cmd.Parameters.AddWithValue("@category", "Obst");
+                cmd.ExecuteNonQuery();
+                dbCon.Close();
+            }
+        }
+    }
+}
